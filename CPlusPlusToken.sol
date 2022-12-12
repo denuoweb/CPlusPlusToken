21fadad664fd8b4e0a177e4b2a80d22f20c18319
Jaron Rosenau <jaron.rosenau@gmail.com>
2022-12-11 23:12:45 -0800

    Update and rename QiToken.sol to CPlusPlusToken.sol

--- CPlusPlusToken.sol
+++ CPlusPlusToken.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.6.0;
 
 import "./openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
-contract QiToken is ERC20 {
+contract CPlusPlusToken is ERC20 {
 	uint256 private _initialSupply=1000000000*10**18;
 
-    constructor() public ERC20("Qi", "QI"){
+    constructor() public ERC20("C++Token", "CPPT"){
         _mint(msg.sender, _initialSupply);
     }
 }
