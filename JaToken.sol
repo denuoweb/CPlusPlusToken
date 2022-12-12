18a93c5b9857ce539be4e0abc4af3812dcb15b1b
Jaron Rosenau <jaron.rosenau@gmail.com>
2022-12-11 23:44:03 -0800

    Update and rename CPlusPlusToken.sol to JaToken.sol

--- JaToken.sol
+++ JaToken.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.6.0;
 
 import "./openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
-contract CPlusPlusToken is ERC20 {
+contract JaToken is ERC20 {
 	uint256 private _initialSupply=1000000000*10**18;
 
-    constructor() public ERC20("C++Token", "CPPT"){
+    constructor() public ERC20("Ja", "JA"){
         _mint(msg.sender, _initialSupply);
     }
 }
